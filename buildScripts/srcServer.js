6a2ef3eded6c90cd5cd1a24220de282fd654daa8
Rodney Santiago <rodney.santiago@ruralsourcing.com>
2019-05-02 23:52:28 -0400

    End of transpiling section. Next section is bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, (err) => {
